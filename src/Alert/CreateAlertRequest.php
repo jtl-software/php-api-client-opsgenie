27b96cf2927e895eb5d65befe0d97bb26fd31ece
mpodliesny <m.podliesny@bitcat.agency>
2021-07-06 17:20:08 +0300

    fix: fixed responders array in CreateAlertRequest, updated guzzle version in composer.json

--- src/Alert/CreateAlertRequest.php
+++ src/Alert/CreateAlertRequest.php
@@ -66,7 +66,7 @@ class CreateAlertRequest implements OpsGenieRequest
         }
 
         foreach ($this->alert->getResponders() as $responder) {
-            $body['responders'] = [
+            $body['responders'][] = [
                 'id' => $responder->getId(),
                 'type' => $responder->getType()
             ];
